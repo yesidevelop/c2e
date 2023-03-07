15e40fa848f412c815814957fa0c70a8639c5cc3
Ali Mehdi <imcoder001@gmail.com>
2023-03-07 20:50:05 +0500

    updated

--- webapp/routes/index.js
+++ webapp/routes/index.js
@@ -38,6 +38,70 @@ router.get('/cms', function(req, res1, next) {
   
   
 });
+
+/* GET cms page. */
+router.get('/authoring', function(req, res1, next) {
+  var options = {
+    host: 'authoring',
+    port: 80,
+    path: '/authoring'
+  };
+  var body = '';
+  var req = http.get(options, function(res) {
+    console.log('STATUS: ' + res.statusCode);
+    console.log('HEADERS: ' + JSON.stringify(res.headers));
+  
+    // Buffer the body entirely for processing as a whole.
+    var bodyChunks = [];
+    res.on('data', function(chunk) {
+      // You can process streamed parts here...
+      bodyChunks.push(chunk);
+    }).on('end', function() {
+      body = Buffer.concat(bodyChunks);
+      console.log('BODY: ' + body);
+      res1.render('index', { title: body });
+      // ...and/or process the entire body here.
+    })
+  });
+  
+  req.on('error', function(e) {
+    console.log('ERROR: ' + e.message);
+  });
+  
+  
+});
+
+/* GET cms page. */
+router.get('/media', function(req, res1, next) {
+  var options = {
+    host: 'media',
+    port: 80,
+    path: '/media'
+  };
+  var body = '';
+  var req = http.get(options, function(res) {
+    console.log('STATUS: ' + res.statusCode);
+    console.log('HEADERS: ' + JSON.stringify(res.headers));
+  
+    // Buffer the body entirely for processing as a whole.
+    var bodyChunks = [];
+    res.on('data', function(chunk) {
+      // You can process streamed parts here...
+      bodyChunks.push(chunk);
+    }).on('end', function() {
+      body = Buffer.concat(bodyChunks);
+      console.log('BODY: ' + body);
+      res1.render('index', { title: body });
+      // ...and/or process the entire body here.
+    })
+  });
+  
+  req.on('error', function(e) {
+    console.log('ERROR: ' + e.message);
+  });
+  
+  
+});
 module.exports = router;
 
 
