a199cb6457ecf2fe48731dd9db958661c8a07e5d
Ali Mehdi <imcoder001@gmail.com>
2023-03-08 12:52:55 +0500

    updated

--- cms/src/controllers/cms.controller.ts
+++ cms/src/controllers/cms.controller.ts
@@ -1,12 +1,23 @@
 // Uncomment these imports to begin using these cool features!
 
-// import {inject} from '@loopback/core';
+import {inject} from '@loopback/core';
 import {get} from '@loopback/rest';
+import {People} from '../services';
 
 export class CmsController {
-  constructor() {}
+  constructor(
+    @inject('services.People')
+    protected peopleService: People,
+  ) { }
   @get('/cms')
   cms(): string {
     return 'Rendering from CMS!';
+  }
+
+  @get('/people')
+  async getCharacter(
+  ): Promise<string> {
+    //Preconditions
+    return this.peopleService.getCharacter();
   }
 }
